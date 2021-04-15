527bd8f5007e45056a57aa57cc843088e0cff7af
Jose Tello <jose.luis.tello@gmail.com>
2021-04-15 00:05:35 -0500

    final

--- main_eecc.sh
+++ main_eecc.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+RutaLand="/home/jtello/OneDrivePersonalProjects/EECC"
+RutaProcesadoPDF="/home/jtello/OneDrivePersonalProjects/EECC/ProcesadoPDF"
+RutaProcesadoCSV="/home/jtello/OneDrivePersonalProjects/EECC/ProcesadoCSV"
+echo "moviendo .PDF a IN_FILES/"
+mv $RutaLand/*.PDF IN_FILES/
+#cp /home/jtello/OneDrivePersonalProjects/EECC/*.PDF /home/jtello/OneDrivePersonalProjects/EECC/IN_FILES/
 for f in IN_FILES/*; do
  ./eecc.sh "$f" $1
+#echo "$f"
 done
+echo "copiando .csv en  $RutaProcesadoCSV"
+cp OUT_FILES/*.csv $RutaProcesadoCSV/
+echo "copiando .PDF en  $RutaProcesadoPDF"
+cp IN_FILES/*.PDF $RutaProcesadoPDF/
+echo "eliminando .csv de OUT_FILES/"
+rm OUT_FILES/*.csv
+echo "eliminando .PDF de IN_FILES/"
+rm IN_FILES/*.PDF
