00264c348334e0aa583d254423e6d1cb8495f4ef
jose <jose.luis.tello@gmail.com>
2021-04-17 23:30:10 -0500

    test

--- main_eecc_pi.sh
+++ main_eecc_pi.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 source rutas.sh
-echo "moviendo .PDF a IN_FILES/"
+echo "$(date) - Moviendo .PDF a IN_FILES/"
 mv $RutaLand/*.PDF IN_FILES/
 for f in IN_FILES/*; do
  ./eecc.sh "$f" $1
-#echo "$f"
 done
-echo "copiando .csv en  $RutaProcesadoCSV"
+echo "$(date) - Copiando .csv en  $RutaProcesadoCSV"
 cp OUT_FILES/*.csv $RutaProcesadoCSV/
-echo "copiando .PDF en  $RutaProcesadoPDF"
+echo "$(date) - Copiando .PDF en  $RutaProcesadoPDF"
 cp IN_FILES/*.PDF $RutaProcesadoPDF/
-echo "eliminando .csv de OUT_FILES/"
+echo "$(date) - Eliminando .csv de OUT_FILES/"
 rm OUT_FILES/*.csv
-echo "eliminando .PDF de IN_FILES/"
+echo "$(date) - Eliminando .PDF de IN_FILES/"
 rm IN_FILES/*.PDF
---
+++ onedrive.sh
@@ -0,0 +1,1 @@
+rclone --vfs-cache-mode writes mount --daemon onedrive:Documents/03_PersonalProyects/EECC ~/Documents/EECC
